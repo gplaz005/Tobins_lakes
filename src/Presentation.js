781bfcb1e4160ebe00b80eb587f698800a538937
gplaz005 <gplaz005@fiu.edu>
2020-09-15 15:33:29 -0400

    added img loader to webpak, display 2 images

--- src/Presentation.js
+++ src/Presentation.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { hot } from 'react-hot-loader';
+import snow from '../snow.png';
+import snow2 from '../snow2.png';
 
 
-const Presentation = () => (
-    <div className="pres">
+const Presentation = () => {
+
+    
+
+    const [counter, setCounter] = useState([snow,snow2]);
+    
+    return(
+        <div className="pres">
         <h1>presentationComponent</h1>
+        <h3>here are your Backdrops:</h3>
+        {counter.map((ki, index)=>
+        <img src ={ki} key = {index}/>
+        )}
+
     </div>
-);
+    );
+
+}
+
+
+       
 
 export default hot(module)(Presentation);
--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,13 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ["style-loader", "css-loader"]
-            }
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                  'file-loader',
+                ],
+              }
         ]
     },
     resolve: { extensions: ['*', '.js', '.jsx'] },
